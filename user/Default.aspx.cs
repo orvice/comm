b96bdcd4a7302403ab044acf671662adf0762cf6
orvice <orvice@gmail.com>
2014-06-04 19:12:34 +0800

    add user/Default.aspx

---
+++ user/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class user_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //如果没有登录，跳转到登录页面。
+        if (Session["username"] == null)
+        {
+            Response.Redirect("user_login.aspx");
+        }
+
+        //已经登录 跳转到 Panel
+        else
+        {
+            Response.Redirect("user_panel.aspx");
+        }
+    }
+}
