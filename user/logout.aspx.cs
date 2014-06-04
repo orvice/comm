7625821698515c3b89cecfa2b292ab85c13ffb45
orvice <orvice@gmail.com>
2014-06-04 17:21:27 +0800

    更改退出登录后进入的地址

--- user/logout.aspx.cs
+++ user/logout.aspx.cs
@@ -10,6 +10,6 @@ public partial class user_logout : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         Session["username"] = null;
-        Response.Redirect("user_login.aspx");
+        Response.Redirect("../index.aspx");
     }
 }
