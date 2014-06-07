65a53f0886f584470dc903d2b963f324505a4af5
orvice <orvice@gmail.com>
2014-06-07 15:29:45 +0800

    update gv

--- user/item_list.aspx.cs
+++ user/item_list.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class user_item_list : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        { gv_bind(); }
+
         //如果没有登录，跳转到登录页面。
         if (Session["username"] == null)
         {
@@ -23,5 +29,38 @@ public partial class user_item_list : System.Web.UI.Page
             //Label2.Text = Session["user_fullname"].ToString();
             //Label3.Text = Session["user_id"].ToString();
         }
+    }
+
+    public void gv_bind()
+    {
+        string user_id = Session["user_id"].ToString();
+
+        //connect to sql server 连接到数据库
+        string s = ConfigurationManager.ConnectionStrings["siteconn"].ConnectionString;
+        SqlConnection conn = new SqlConnection(s);
+        //打开数据库连接
+        //conn.Open();
+        string gv_sql = "select * from comm_item_log WHERE user_id = '"+ user_id+ "' ";
+
+        SqlDataAdapter gv = new SqlDataAdapter(gv_sql,conn);
+        DataSet ds = new DataSet();
+
+        try
+        {
+            conn.Open();
+            gv.Fill(ds, "comm_item_log");
+            GV1.DataSource = ds.Tables["comm_item_log"].DefaultView;
+            GV1.DataKeyNames = new string[] { "item_id" };
+            GV1.DataBind();
+        }
+        catch (Exception ex)
+        {
+            Response.Write(ex);
+        }
+        finally
+        {
+            conn.Close();
+        }
+
     }
 }
