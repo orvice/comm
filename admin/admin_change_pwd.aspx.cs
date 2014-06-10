8122f0cccd769436c547f0cb7b34ed69d8ce48de
orvice <orvice@gmail.com>
2014-06-10 20:12:18 +0800

    finish pwd update func

--- admin/admin_change_pwd.aspx.cs
+++ admin/admin_change_pwd.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 public partial class admin_admin_change_pwd : System.Web.UI.Page
 {
@@ -22,5 +25,30 @@ public partial class admin_admin_change_pwd : System.Web.UI.Page
             Label1.Text = Session["admin"].ToString();
             //Label2.Text = Session["admin_fullname"].ToString();
         }
+    }
+
+    protected void change_pwd_botton_Click(object sender, EventArgs e)
+    {
+        //获取文本框变量        
+        string new_pwd = new_pwd_text.Text.ToString().Trim();
+        string admin_id = Session["admin_id"].ToString();
+        //string user_old_pwd = old_pwd_text.Text.ToString().Trim();
+        //string admin_name = Session["admin_name"].ToString().Trim();
+
+        //connect to sql server 连接到数据库
+        string s = ConfigurationManager.ConnectionStrings["siteconn"].ConnectionString;
+        SqlConnection conn = new SqlConnection(s);
+
+        //更新密码到数据库           
+        //打开数据库连接
+        conn.Open();
+        string change_pwd = "UPDATE  [comm_admin] SET admin_pwd= '" + new_pwd + "' WHERE admin_id='" + admin_id + "' ";
+        SqlCommand cmd_change_pwd = new SqlCommand(change_pwd, conn);
+        cmd_change_pwd.ExecuteNonQuery();
+        //关闭数据库连接并返回结果
+        conn.Close();
+        Response.Write("<script language=javascript>alert('修改成功 ')</script>");
+        //Response.Redirect("item_panel.aspx");
+
     }
 }
