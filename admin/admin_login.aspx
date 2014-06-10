b614845d05462e0b9db7bf98ebb7f01148b915bc
orvice <orvice@gmail.com>
2014-06-10 19:06:50 +0800

    add file

---
+++ admin/admin_login.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="admin_login.aspx.cs" Inherits="admin_login" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+<!-- Loading Bootstrap -->
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="../css/flat-ui.css" rel="stylesheet">
+    <link href="../css/demo.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="../images/favicon.ico">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="../js/html5shiv.js"></script>
+      <script src="../js/respond.min.js"></script>
+    <![endif]-->
+
+
+    <title>管理员登录</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="container" >
+    
+
+
+
+  <div class="login">
+        <div class="login-screen">
+          <div class="login-icon">
+            <img src="../images/login/icon.png" alt="Welcome to Mail App" />
+            <h4>欢迎使用 <small>管理面板</small></h4>
+          </div>
+
+          <div class="login-form">
+            <div class="form-group">
+               
+
+               <asp:TextBox class="form-control login-field" value="" placeholder="输入用户名"  ID="username" runat="server"></asp:TextBox></p>
+              <label class="login-field-icon fui-user" for="login-name"></label>
+            </div>
+
+            <div class="form-group">
+               
+               <asp:TextBox type="password" class="form-control login-field" value="" placeholder="输入密码" ID="userpwd" runat="server"></asp:TextBox></p>
+              <label class="login-field-icon fui-lock" for="login-pass"></label>
+            </div>
+
+             
+            <asp:Button ID="login_botton" class="btn btn-primary btn-lg btn-block" runat="server" Text="登录" onclick="login_botton_Click" />
+            <!--<a class="login-link" href="user_reg.aspx">没有账号?点击这里注册</a> -->
+          </div>
+        </div>
+  </div>
+
+      </div>
+    </form>
+</body>
+</html>
---
+++ admin/admin_login.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class admin_login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void login_botton_Click(object sender, EventArgs e)
+    {
+        //获取登陆框的用户名和密码
+        string login_user_name = username.Text.ToString().Trim();
+        string login_user_pwd = userpwd.Text.ToString().Trim();
+        //connect to sql server 连接到数据库
+        string s = ConfigurationManager.ConnectionStrings["siteconn"].ConnectionString;
+        SqlConnection conn = new SqlConnection(s);
+        //打开数据库连接
+        conn.Open();
+        //查询用户名
+        string s_1 = " select * from [comm_user] where user_name= '" + login_user_name + " ' ";
+        SqlCommand cmd_1 = new SqlCommand(s_1,conn);
+        SqlDataReader dr = cmd_1.ExecuteReader();
+
+        if ( dr.Read() )
+        {
+            //用户名存在
+            string tmp_pwd       = dr["user_pwd"].ToString();
+            string user_fullname = dr["user_fullname"].ToString();
+            string user_id       = dr["id"].ToString();
+            if (login_user_pwd.Trim() != tmp_pwd.Trim())
+            {
+                //密码错误
+                Response.Write("<script language=javascript>alert('密码错误 ')</script>");
+            }
+            else
+            {
+                //密码正确
+                //Response.Write("<script language=javascript>alert('密码正确')</script>");
+
+                //登录成功
+                Session["username"] = login_user_name;
+                Session["user_fullname"] = user_fullname;
+                Session["user_id"] = user_id;
+                Response.Redirect("user_panel.aspx");
+                conn.Close();
+
+            }
+        }
+
+        else
+        {
+            //用户名不存在
+            Response.Write("<script language=javascript>alert('用户不存在')</script>");
+        }
+
+
+        //用户名or密码未输入
+        if (login_user_name == "" || login_user_pwd == "")
+        {
+            Response.Write("<script language=javascript>alert('用户名或密码未输入')</script>");
+
+        }
+        
+
+    }
+}
