61fbc418c9ba09764e88b486d622a219c1d3dfa7
orvice <orvice@gmail.com>
2014-06-10 19:20:12 +0800

    update page load

--- admin/Default.aspx.cs
+++ admin/Default.aspx.cs
@@ -9,6 +9,16 @@ public partial class admin_Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //如果没有登录，跳转到登录页面。
+        if (Session["admin"] == null)
+        {
+            Response.Redirect("admin_login.aspx");
+        }
 
+        //已经登录 跳转到 Panel
+        else
+        {
+            Response.Redirect("admin_panel.aspx");
+        }
     }
 }
