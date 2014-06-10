f00b59f114ddc706e3fc7660a6cfcb1821808a88
orvice <orvice@gmail.com>
2014-06-10 21:41:26 +0800

    finish Page_Load

--- user/usre_profile.aspx.cs
+++ user/usre_profile.aspx.cs
@@ -25,10 +25,35 @@ public partial class user_usre_profile : System.Web.UI.Page
             Label1.Text = Session["username"].ToString();
             //Label2.Text = Session["user_fullname"].ToString();
             //Label3.Text = Session["user_id"].ToString();
+
+            //获取用户ID和UserName
+            string user_id = Session["user_id"].ToString();
+            string user_name_tmp = Session["username"].ToString();
+            string user_name = user_name_tmp.ToString().Trim();
+
+            //connect to sql server 连接到数据库
+            string s = ConfigurationManager.ConnectionStrings["siteconn"].ConnectionString;
+            SqlConnection conn = new SqlConnection(s);
+            //打开数据库连接
+            conn.Open();
+            //查询用户名
+            string s_1 = " select * from [comm_user] where user_name= '" + user_name + " ' ";
+            SqlCommand cmd_1 = new SqlCommand(s_1, conn);
+            SqlDataReader dr = cmd_1.ExecuteReader();
+            dr.Read();
+            //将数据库的内容copy到textbox
+            username.Text = Session["username"].ToString();
+            fullname.Text = dr["user_fullname"].ToString();
+            email.Text = dr["user_email"].ToString();
+            add.Text = dr["user_add"].ToString();
+            info.Text = dr["user_info"].ToString();
+            //关闭数据库连接
+            conn.Close();
         }
     }
 
     protected void update_botton_Click(object sender, EventArgs e)
     { 
+
     }
 }
